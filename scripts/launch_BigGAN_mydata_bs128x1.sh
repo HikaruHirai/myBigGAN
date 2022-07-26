5cca885cebacdfe4b97b119ac60285b0045dbc5f
HikaruHirai <h.hikaru0223@gmail.com>
2022-07-27 00:44:51 +0900

    scripts

--- scripts/launch_BigGAN_mydata_bs128x1.sh
+++ scripts/launch_BigGAN_mydata_bs128x1.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 python train.py \
---dataset myData --parallel --shuffle  --num_workers 8 --batch_size 128 --load_in_mem  \
+--dataset myDataset --parallel --shuffle  --num_workers 8 --batch_size 128 --load_in_mem  \
 --num_G_accumulations 1 --num_D_accumulations 1 \
 --num_D_steps 1 --G_lr 1e-4 --D_lr 4e-4 --D_B2 0.999 --G_B2 0.999 \
 --G_attn 64 --D_attn 64 \
