bef041841a2581330a6984119a8909b76905470e
HikaruHirai <h.hikaru0223@gmail.com>
2022-07-28 22:48:50 +0900

    update prepare_data_colab.sh

--- scripts/utils/prepare_data_colab.sh
+++ scripts/utils/prepare_data_colab.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-python make_hdf5.py --dataset myDataset --batch_size 128 --data_root /content/drive/MyDrive/images/img
-python calculate_inception_moments.py --dataset myDataset_hdf5 --data_root /content/drive/MyDrive/images/img
+python make_hdf5.py --dataset myDataset --batch_size 128 --data_root /content/drive/MyDrive/images
+python calculate_inception_moments.py --dataset myDataset_hdf5 --data_root /content/drive/MyDrive/images
