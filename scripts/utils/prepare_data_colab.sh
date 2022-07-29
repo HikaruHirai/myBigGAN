fb3bcddaf8c0ff1f955d98dce481f43768c891b9
HikaruHirai <h.hikaru0223@gmail.com>
2022-07-29 22:46:43 +0900

    update prepare_data_colab.sh and launch_BigGAN_mydata_bs128x1

--- scripts/utils/prepare_data_colab.sh
+++ scripts/utils/prepare_data_colab.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-python make_hdf5.py --dataset myDataset --data_root /content/drive/MyDrive/images --batch_size 128 --num_workers 0
+python make_hdf5.py --dataset myDataset --data_root /content/drive/MyDrive/images --batch_size 64 --num_workers 0
 python calculate_inception_moments.py --dataset myDataset --data_root /content/drive/MyDrive/images --num_workers 0
