71fd2df2e18a04aa079a380d473534fd10d82d5d
HikaruHirai <h.hikaru0223@gmail.com>
2022-08-08 21:23:55 +0900

    update train script

--- scripts/launch_BigGAN_mydata_bs128x1_resume.sh
+++ scripts/launch_BigGAN_mydata_bs128x1_resume.sh
@@ -20,3 +20,4 @@ python train.py \
 --weights_root /content/drive/MyDrive/models/weights \
 --logs_root /content/drive/MyDrive/models/logs \
 --samples_root /content/drive/MyDrive/models/samples \
+--total 811 \
